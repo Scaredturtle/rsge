fea31ee10f8a014aa93959fe664a84f3bbb0f396
scaredturtle <demarcam2012@yahoo.com>
2020-08-23 12:51:24 -0400

    Outline Created

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+#!usr/bin/env python3
+
+'''
+This is the main file for a project that is designed to grab specific item 
+information from the Runescape Grand Exchange.
+
+Current goals are to successfully pull the data from the site.
+'''
+
+import requests
+from bs4 import BeautifulSoup as bs
+
 print ("Hello World!")
